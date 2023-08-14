dbd423f561899227744ba82681aa05d56cd21fd8
Nirob Hasan <nirob427417@gmail.com>
2023-08-14 11:54:27 +0600

    category & post

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from "cors";
 import express, { Application } from "express";
+import { CategoryRoutes } from "./modules/category/category.route";
+import { PostRoutes } from "./modules/post/post.route";
 import { UserRoutes } from "./modules/user/user.route";
 
 const app: Application = express();
@@ -8,5 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/user", UserRoutes);
+app.use("/api/v1/category", CategoryRoutes);
+app.use("/api/v1/post", PostRoutes);
 
 export default app;
---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,18 @@
+import { Post, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const cratePost = async (data: Post): Promise<Post> => {
+  const result = await prisma.post.create({
+    data,
+    include: {
+      author: true,
+      category: true,
+    },
+  });
+  return result;
+};
+
+export const PostService = {
+  cratePost,
+};
